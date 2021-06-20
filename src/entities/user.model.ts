5a90898d994e35becf9027a57843743c81a9bd68
Aliaksei <alex.latypov94@gmail.com>
2021-06-20 15:31:34 +0300

    feat: create models

---
+++ src/entities/user.model.ts
@@ -0,0 +1,40 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { IUser } from '../interfaces';
+
+type IUserForReponse = Omit<IUser, 'password'>;
+
+/**
+ * Class representing an user
+ */
+@Entity()
+export class User implements IUser {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ length: 100 })
+  name: string;
+
+  @Column({ length: 100 })
+  login: string;
+
+  @Column({ length: 100 })
+  password: string;
+
+  constructor({
+    id = uuid(),
+    name = 'USER',
+    login = 'user',
+    password = 'P@55w0rd',
+  }: Partial<IUser> = {}) {
+    this.id = id;
+    this.name = name;
+    this.login = login;
+    this.password = password;
+  }
+
+  static toResponse(user: IUser): IUserForReponse {
+    const { id, name, login } = user;
+    return { id, name, login };
+  }
+}
